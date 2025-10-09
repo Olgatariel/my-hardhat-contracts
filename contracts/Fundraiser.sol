1ee0ef02e5f2b5e47de2eb2c002f348a13e8b728
Olya Tariielashvili <olya@Olyas-MacBook-Pro.local>
2025-10-09 23:30:44 +0200

    added full Fundraiser contract code

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -9,4 +9,66 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 // ----------------------
 // Contract
 // ----------------------
-contract Milestone is Ownable, ReentrancyGuard, Pausable {}
+contract Fundraiser is Ownable, ReentrancyGuard, Pausable {
+    struct Campaign {
+        uint id;
+        string description;
+        uint goal;
+        uint raised;
+        bool completed;
+        address payable owner;
+    }
+    // ----------------------
+    // State variables
+    // ----------------------
+
+    mapping(uint => Campaign) public campaigns;
+    uint public nextId;
+    uint public totalRaised;
+
+    //----------------------
+    //Error
+    //----------------------
+    error InvalidGoal();
+    error InvalidCampaignId();
+    error InvalidDonationAmount();
+    error AlreadyCompleted();
+
+    // ----------------------
+    // Events
+    // ----------------------
+
+    event CampaignCreated(uint indexed id, address indexed owner, uint goal);
+    event DonationReceived(uint indexed id, address indexed from, uint amount);
+
+    // ----------------------
+    // Functions
+    // ----------------------
+
+    function createCampaign(
+        string memory _description,
+        uint _goal
+    ) public whenNotPaused {
+        if (_goal == 0) revert InvalidGoal();
+        Campaign memory newCampaign = Campaign({
+            id: nextId,
+            description: _description,
+            goal: _goal,
+            raised: 0,
+            completed: false,
+            owner: payable(msg.sender)
+        });
+        campaigns[nextId] = newCampaign;
+        emit CampaignCreated(nextId, msg.sender, _goal);
+        nextId++;
+    }
+    function donate(uint _id) external payable whenNotPaused nonReentrant {
+        if (_id >= nextId) revert InvalidCampaignId();
+        if (campaigns[_id].completed) revert AlreadyCompleted();
+        if (msg.value == 0) revert InvalidDonationAmount();
+        campaigns[_id].raised += msg.value;
+        totalRaised += msg.value;
+
+        emit DonationReceived(_id, msg.sender, msg.value);
+    }
+}
